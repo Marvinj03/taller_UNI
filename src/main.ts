ad94188b332ce0d5813c89cf1f785a7c9a352fd4
Marvinj03 <44850409+Marvinj03@users.noreply.github.com>
2025-11-23 17:41:32 -0600

    actualización de main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,19 @@
-
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  
+  app.enableCors();
 
   const config = new DocumentBuilder()
-    .setTitle('API CRUD _ TALLER UNI')
-    .setDescription('CRUD basico demo de despliegue')
+    .setTitle('API de Productos por Marvin Josué Pérez Vanegas') // Título Personalizado
+    .setDescription('Backend de demostración de CI/CD (Taller UNI), desarrollado y desplegado por Marvin Josué Pérez Vanegas. Proporciona la gestión completa (CRUD) de productos.') // Descripción Personalizada
     .setVersion('1.0')
-    .addTag('APICRUD')
+    .addTag('productos', 'Operaciones CRUD para la entidad Producto') 
     .build();
+  
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
